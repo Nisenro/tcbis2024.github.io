0b543f1d7073ec02b241ca774c79350a71547a5e
Nisenro <adegbenroadeseke@gmail.com>
2024-08-17 10:20:49 +0100

    created a new spider

---
+++ ieee/ieee/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class IeeeScrapyItem(scrapy.Item):
+    title = scrapy.Field()
+    date = scrapy.Field()
+    location = scrapy.Field()
+    country = scrapy.Field()
+    link = scrapy.Field()
